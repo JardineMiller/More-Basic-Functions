a56f140ff79a44ec6a9e5140a9d156accff5781d
Jardine <jardine.miller@gmail.com>
2017-10-31 19:07:45 +0000

    Completed tests

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -29,3 +29,61 @@ end
 def add_string_as_number(s1, s2)
 	return s1.to_i + s2.to_i
 end
+
+def number_to_full_month_name(number)
+	case number
+		when 1
+			"January"
+		when 2
+			"February"
+		when 3
+			"March"
+		when 4
+			"April"
+		when 5
+			"May"
+		when 6
+			"June"
+		when 7
+			"July"
+		when 8
+			"August"
+		when 9
+			"September"
+		when 10
+			"October"
+		when 11
+			"November"
+		when 12
+			"December"
+	end
+end
+
+def number_to_short_month_name(number)
+	case number
+		when 1
+			"Jan"
+		when 2
+			"Feb"
+		when 3
+			"Mar"
+		when 4
+			"Apr"
+		when 5
+			"May"
+		when 6
+			"Jun"
+		when 7
+			"Jul"
+		when 8
+			"Aug"
+		when 9
+			"Sep"
+		when 10
+			"Oct"
+		when 11
+			"Nov"
+		when 12
+			"Dec"
+	end
+end
