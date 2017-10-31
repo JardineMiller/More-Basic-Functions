1aefbc81f60f31cfa5a69c5f64968231fb7a6e31
Jardine <jardine.miller@gmail.com>
2017-10-31 16:23:34 +0000

    Initial commit

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -5,3 +5,27 @@ end
 def add(num1, num2)
 	return num1 + num2
 end
+
+def subtract(num1, num2)
+	return num1 - num2
+end	
+
+def multiply(n1, n2)
+	return n1 * n2
+end
+
+def divide(n1, n2)
+	return n1 / n2
+end	
+
+def length_of_string(string)
+	string.length
+end
+
+def join_string(string1, string2)
+	return string1 + string2
+end
+
+def add_string_as_number(s1, s2)
+	return s1.to_i + s2.to_i
+end
